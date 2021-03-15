801e8b45ba6d47f3e7b6f927dbfeee49e000e147
Roni Celestino <ronicelestino@gmail.com>
2021-03-15 16:57:46 -0300

    feat: Add news api fetch

--- src/presentation/pages/noticias.js
+++ src/presentation/pages/noticias.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import App from '../../App'
 
 const Noticia = () => {
+  const [post, setPost] = useState([])
+  const url = "http://localhost:5000/api/post/orgao/1"
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const response = await fetch(url, {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        const data = await response.json()
+        setPost(data.post)
+        return
+      } catch (err) {
+        console.log(err.message)
+      }
+    })()
+  }, [url])
+
+
   return (
     <App>
-        <h1>Noticia</h1>
+      <ul>
+      {post.map((item) => 
+        <li key={item.id}>
+         <div>{item.title}</div>
+         <div>{item.post_content}</div>
+         <div>{item.}</div>
+        </li>
+      )}
+      </ul>
     </App>
   )
 }
